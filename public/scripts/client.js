189a5773af99e24310b564671122d505c3f23e1a
Taha Helwani <telhe008@uottawa.ca>
2021-11-20 03:35:59 +0000

    Removed comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
 
 const renderTweets = function(tweets) {
   // loops through tweets
@@ -90,7 +65,6 @@ const generateError = (message) => {
   .html(`<i class='fa fa-warning' style='color:red'></i> ${message} <i class='fa fa-warning' style='color:red'>`)
   .slideDown('fast');
 }
-// Test / driver code (temporary)
 $(() => {
   loadtweets();
   // renderTweets(data);
